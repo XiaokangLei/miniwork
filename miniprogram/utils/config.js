26cf3b7c4f2e3f1fda4cdb3aa6a4e5e27e2fb427
leixk_ai <leixk_ai@si-tech.com.cn>
2021-08-04 22:08:36 +0800

    update

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -3,23 +3,10 @@
   小贝校招
   仅供学习交流，如作它用所承受的法律责任一概与作者无关
   使用ColorUi开发扩展与插件时，请注明基于ColorUi开发
-  （QQ交流群：240787041）
 */
 
 var envId = 'test-4gn9gu0ucc6657ba'
-
-// Config.NODE_ENV = false;// 是否使用云数据，布尔类型参数
-// Config.NODE_envcloud="test-4gn9gu0ucc6657ba"// 云环境地址，布尔类型参数
-
-// Config.HOST_DEBUG_URI = 'http://192.168.1.140:8080';
-// // Config.HOST_DEBUG_URI = 'http://192.168.1.137:5000';
-// // 是否连接本地测试服true
-// Config.debug_local = true;
-// Config.recharge_card_tab = []; //乐享卡信息
-// Config.checked_customer_id = ""; //选择客户ID
-// Config.checked_customer_name = ""; //选择客户名
-// Config.no_img = "no_img.png";//默认商品图片
-// 获取微信用户小程序open_id及用户信息
+// var envId = 'product-2gy06ytxb826f95d'
 
 module.exports={
   envId:envId
